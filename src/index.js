ac868dbe4f1004f33ab5a160fac4699932a57e62
Krishan Gopal <99832785+MKrishan21@users.noreply.github.com>
2025-08-08 16:49:34 +0530

    added patch to api gateway

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const fastify = Fastify({
 
 await fastify.register(cors, {
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
 });
 
 
